f9852e3b8b45d682a75252a37db026e6555ffb88
TPdkr <tpodko@gmail.com>
2026-02-24 10:07:56 +0100

    overlay as an option added for plot cols on map and used in the visualizatiosn file to compare predictions to training data.

--- new_approach/visualize.py
+++ new_approach/visualize.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import seaborn as sns
+import pandas as pd
 
 def plotColsOnMap(
         cols,
         df, 
+        overlay=None,
         log_range = False, 
         constant = (10**6), 
         cmap="viridis",
@@ -24,6 +26,7 @@ def plotColsOnMap(
     Args:
         cols: list of column names
         df: pandas dataframe
+        overlay: pandas dataframe to overlay if not None(default)
         log_range: True/False(defualt False)
         constant: constant by which to mutliply all values(default 10**6)
         cmap: default"viridis"
@@ -39,30 +42,42 @@ def plotColsOnMap(
         nothing
 
     """
+    isOverlay = isinstance(overlay, pd.DataFrame)
+
     #the index is reset from using lat and lon just in case
     df_reset = df.reset_index()
+    dfo_reset = overlay.reset_index() if isOverlay else None
 
-    #the number of rows varies depending on the number of columns to plot
+    #the number of rows varies depending on the number of columns to plot and subplots created
     rows = (len(cols)//2) + (len(cols)%2==1)
-
-    #a set of subplots is created
     fig, axes = plt.subplots(nrows=rows, ncols=2, figsize=(20, rows*4), subplot_kw={"projection": ccrs.PlateCarree()})
 
     #we plot each column
     axes = axes.flatten()
+
     for i, col in enumerate(cols):
         ax = axes[i]#subplot
 
         #we want to see the coastlines on the globe and only take not null values
         ax.add_feature(cfeature.COASTLINE)
         valid_data = df_reset[df_reset[col].notna()]
+        valid_overlay = dfo_reset[dfo_reset[col].notna()] if isOverlay else None
 
         #the depth when specified limits the depth visualized to a specifc depth
         if specific_depth != None:
+            #base layer containing most data
             mask = valid_data["DEPTH (m)"]==specific_depth
             valid_data=valid_data[mask]
+
+            if (isOverlay):
+                #overlay layer
+                mask = valid_overlay["DEPTH (m)"]==specific_depth
+                valid_overlay=valid_overlay[mask]
         else:
             valid_data = valid_data.groupby(by=['LATITUDE', 'LONGITUDE']).mean().reset_index()
+
+            if (isOverlay):
+                valid_overlay=valid_overlay.groupby(by=['LATITUDE', 'LONGITUDE']).mean().reset_index()
 
         #this sets the logorithmic scale to be exactly like in the paper instead of default
         norm = None        
@@ -79,8 +94,23 @@ def plotColsOnMap(
             transform=ccrs.PlateCarree(),
             norm=norm,
             vmin=min_lim,
-            vmax=max_lim
+            vmax=max_lim,
+            zorder=1
         )
+
+        if (isOverlay):
+            sc_over = ax.scatter(
+                valid_overlay["LONGITUDE"],
+                valid_overlay["LATITUDE"],
+                c=transf(((valid_overlay[col])*constant)),#data is multiplied by a constant and transformed
+                cmap=cmap,
+                s=3*s,
+                transform=ccrs.PlateCarree(),
+                norm=norm,
+                vmin=min_lim,
+                vmax=max_lim,
+                zorder=2
+            )
 
         #we want to see the entire globe and not just the values 
         ax.set_xlim(-180,180)
